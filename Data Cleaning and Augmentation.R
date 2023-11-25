2a427b7af49321dc22f9c22b9ce0ab84d216ea9c
samhar5 <150091351+samhar5@users.noreply.github.com>
2023-11-25 15:30:38 -0800

    Add files via upload
    Initial upload of main CSV's and the data cleaning document

---
+++ Data Cleaning and Augmentation.R
@@ -0,0 +1,16 @@
+#Data Cleaning
+
+library(dplyr)
+
+# Read the CSV files
+tableOne <- read.csv("cybersecurity_attacks.csv")
+tableTwo <- read.csv("cyberthreat.csv")
+
+# Merge based on the common column titled "Protocol"
+result <- merge(tableOne, tableTwo, by = "Protocol", all = TRUE)
+
+# Select only the "Protocol" column in the result
+result <- result["Protocol"]
+
+# View the result
+print(result)
