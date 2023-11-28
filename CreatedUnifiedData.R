143d70dbd2772370d0115d304fd423b5e095a86c
Kbair1 <146391369+Kbair1@users.noreply.github.com>
2023-11-27 21:14:03 -0800

    Update CreatedUnifiedData.R

--- CreatedUnifiedData.R
+++ CreatedUnifiedData.R
@@ -10,5 +10,8 @@ result <- merge(tableOne, tableTwo, by = "Protocol", all = TRUE)
 # Select only the "Protocol" column in the result
 result <- result["Protocol"]
 
+# Limit the output to 25,000 rows or fewer
+result <- head(result, n = 20000)
+
 # View the result
 print(result)
