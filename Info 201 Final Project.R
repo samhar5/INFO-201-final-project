4d0f66cf0cc51d5a610ce03d114248fd614b9cb9
samhar5 <150091351+samhar5@users.noreply.github.com>
2023-11-27 23:54:42 -0800

    Add files via upload
    Finished data cleaning sections (had to use upload via browser because terminal was not working).

--- Info 201 Final Project.R
+++ Info 201 Final Project.R
@@ -7,6 +7,10 @@ tableTwo <- read.csv("cyberthreat.csv")
 # Merge the two different files based on the common column titled "Protocol"
 result <- merge(tableOne, tableTwo, by = "Protocol", all = TRUE)
 
+# Randomly sample 20,000 rows instead of first 20000 rows
+result <- result %>%
+  sample_n(20000)
+
 # Create a new categorical variable - protocol type
 # creates a counter for each type
 catVar <- table(result$Protocol)
@@ -22,6 +26,7 @@ avgPacketSizeThreat <- mean(tableTwo$Packet.Size)
 # Calculate average packet size for both tables combined
 avgPacketSizeBothTable <- mean(c(tableOne$Packet.Length, tableTwo$Packet.Size))
 
+
 # Add the new continuous/numerical variables to the result dataset
 result <- mutate(result,
                  AvgPacketSizeSecurity = avgPacketSizeSecurity,
@@ -29,16 +34,30 @@ result <- mutate(result,
                  AvgPacketSizeBothTable = avgPacketSizeBothTable
 )
 
+# New categorical variable that generalizes packet size
+result <- mutate(result,
+                 AvgPacketPerEvent = (((result$Packet.Length)+(result$Packet.Size))/2))
+
+for (i in 1:20000) {
+  if (isTRUE(result$AvgPacketPerEvent[i] > result$AvgPacketSizeBothTable[i]) == TRUE) {
+    result$packet.measure[i] <- "Large"
+  } else {
+    result$packet.measure[i] <- "Small"
+  }
+}
+
+# New continuous variable that compares individual anomaly scores to the average
+avg_anom <- mean(result$Anomaly.Scores)
+
+result <- mutate(result,
+                 Anom_Avg_Perc_Diff = round((((result$Anomaly.Scores)-avg_anom)/avg_anom) *100))
+
 # Create at least one summarization data frame
 # Summarize attack styles and find the maximum attack type
 attackStyleSummary <- summarise(group_by(tableOne, Attack.Type), Count = n())
 
 # Add the Count variable to the result dataset
 result <- left_join(result, attackStyleSummary, by = "Attack.Type")
-
-# Randomly sample 20,000 rows instead of first 20000 rows
-result <- result %>%
-  sample_n(20000)
 
 # View the updated result
 print(result)
