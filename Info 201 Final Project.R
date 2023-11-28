1bd8edcbb51a0d2a9f171bc8d24d6bd160f19220
Kbair1 <146391369+Kbair1@users.noreply.github.com>
2023-11-27 21:58:15 -0800

    Update Info 201 Final Project.R

--- Info 201 Final Project.R
+++ Info 201 Final Project.R
@@ -11,9 +11,8 @@ result <- merge(tableOne, tableTwo, by = "Protocol", all = TRUE)
 # creates a counter for each type
 catVar <- table(result$Protocol)
 
-# Print the results
-cat("Categorical Variable: Protocol:\n")
-print(catVar)
+# Add the new categorical variable to the result dataset
+result <- mutate(result, CatVariable = catVar[match(result$Protocol, names(catVar))])
 
 # Create at least one new continuous/numerical variable
 # Calculate average packet size for each dataset
@@ -23,20 +22,23 @@ avgPacketSizeTableTwo <- mean(tableTwo$Packet.Size)
 # Calculate average packet size for both tables combined
 avgPacketSizeBothTable <- mean(c(tableOne$Packet.Length, tableTwo$Packet.Size))
 
+# Add the new continuous/numerical variables to the result dataset
+result <- mutate(result,
+                 AvgPacketSizeTableOne = avgPacketSizeTableOne,
+                 AvgPacketSizeTableTwo = avgPacketSizeTableTwo,
+                 AvgPacketSizeBothTable = avgPacketSizeBothTable
+)
+
 # Create at least one summarization data frame
 # Summarize attack styles and find the maximum attack type
 attackStyleSummary <- summarise(group_by(tableOne, Attack.Type), Count = n())
 
-maxAttackType <- attackStyleSummary[which.max(attackStyleSummary$Count), "Attack.Type"]
+# Add the Count variable to the result dataset
+result <- left_join(result, attackStyleSummary, by = "Attack.Type")
 
-# Print the results
-cat("\nNew Continuous/Numerical Variable (Average Packet Size):\n")
-cat("Average Packet Size in tableOne:", avgPacketSizeTableOne, "\n")
-cat("Average Packet Size in tableTwo:", avgPacketSizeTableTwo, "\n")
-cat("Overall Average Packet Size in Both Tables:", avgPacketSizeBothTable, "\n")
+# Randomly sample 20,000 rows instead of first 20000 rows
+result <- result %>%
+  sample_n(20000)
 
-cat("\nSummarization Data Frame:\n")
-print(attackStyleSummary)
-
-cat("\nMost Common Attack Type:\n")
-print(maxAttackType)
+# View the updated result
+print(result)
