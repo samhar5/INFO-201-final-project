de09a89e567af47a9c47f7df23fc6c9aa8c94e63
samhar5 <150091351+samhar5@users.noreply.github.com>
2023-12-10 13:37:48 -0800

    Update Final Project Deliverable.R
    Added 3 analysis sections.

--- Final Project Deliverable.R
+++ Final Project Deliverable.R
@@ -5,6 +5,94 @@ library(readr)
 # Read the dataset
 cyb_df <- read_csv("Cleaned-Data.csv")
 
+tab_danger <- fluidPage(
+  h3("How dangerous is each attack?"), 
+  h5("Let's take a look at the different attack types and how their severity levels vary."),
+  sidebarLayout(
+    sidebarPanel(
+      position = "left", 
+      selectInput(
+        inputId = "malw_type",
+        label = "Attack Types",
+        choices = c("SQL Injection", "Zero-Day Exploits", "Cross-Site Scripting", "Phishing", "Password Attacks", 
+                    "DoS", "Ransomware", "Man-in-the-Middle", "DDoS"),
+        selected = "Phishing"
+      ),
+      helpText(
+        "Here, you can see that most attacks do have a higher level of severity, although there are some attack
+       types that do have a majority of their attacks on the lower severity side."
+      )
+    ),
+    
+    mainPanel(
+      #graph
+      plotOutput(outputId = "malware_sign")
+    )
+  )
+)
+
+tab_anomaly <- fluidPage(
+  h3("Is there a correlation between anomaly score and the severity of an attack?"),
+  h5("Here, we will be focusing on Phishing attacks in particular, as it is the most common attack type and has the most high severity attacks."),
+  h5("An Anomaly Score is the measure of how 'odd' the code of an attack is. That is, it is a measurement of how different or odd the system was acting 
+  from its normal operating behavior."), 
+  h5("Choose a severity level below to see the anomaly score distribution."),
+  sidebarLayout(
+    sidebarPanel(
+      position = "right",
+      radioButtons(
+        inputId = "severe_type",
+        label = "Severity Level of Attack",
+        choices = c("Low", "Medium", "High"),
+        selected = "Low"
+      )
+    ),
+    mainPanel(
+      plotOutput(outputId = "severe_corr")
+    )
+  )
+)
+
+tab_malware <- fluidPage(
+  h3("How often is malware detected?"),
+  h5("Are there factors of an attack that correlate to how often the system detects the malware? Here, we will 
+     be looking at how the different factors of an attack, such as its anomaly score and packet size, affect
+     the system's ability to detect it."),
+  sidebarLayout(
+    sidebarPanel(
+      position = "right",
+      sliderInput(
+        inputId = "anom_score",
+        label = "Choose a range of anomaly scores",
+        min = 0,
+        max = 100,
+        value = c(0,100)
+      ),
+      sliderInput(
+        inputId = "pckg.amt",
+        label = "Choose the maximum package size",
+        min = 96,
+        max = 1260,
+        value = 150
+      )
+    ),
+    mainPanel(
+      h3("Malware Detected based on Anomaly Score"),
+      
+      tableOutput(
+        outputId = "phish_anom"
+      ),
+      
+      h3("Malware Detected based on Package Size"),
+      
+      tableOutput(
+        outputId = "phish_pckg"
+      )
+    )
+  ),
+  h5("Although the values are very close together, we can see that consistently malware is detected less often.")
+)
+
 ui <- fluidPage(
   tabsetPanel(
     tabPanel("Introduction", 
@@ -47,15 +135,9 @@ ui <- fluidPage(
              plotOutput(outputId = "bar_chart_attack_type"),
              plotOutput(outputId = "bar_chart_target_variable")
     ),
-    tabPanel("Tab 3", 
-             # Add content for Tab 3 here
-    ),
-    tabPanel("Tab 4", 
-             # Add content for Tab 4 here
-    ),
-    tabPanel("Tab 5", 
-             # Add content for Tab 4 here
-    )
+    tabPanel("How Dangerous is an Attack?", tab_danger),
+    tabPanel("How Severe is Weird?", tab_anomaly),
+    tabPanel("Malware Detection", tab_malware)
   )
 )
 
@@ -81,6 +163,60 @@ server <- function(input, output) {
       theme_minimal() +
       theme(axis.text.x = element_text(angle = 45, hjust = 1))
   })
+  
+output$malware_sign <- renderPlot({
+    malw_df <- filter(cyb_df, Target.Variable  == input$malw_type, Target.Variable != "NA") %>% group_by(Target.Variable) %>% count(Severity.Level)
+    malw_df$Severity.Level <- factor(malw_df$Severity.Level, levels = c("Low", "Medium", "High"))  
+    
+    ggplot(data = malw_df) + 
+      geom_bar(mapping = aes(x = Severity.Level, y = n, fill = Severity.Level), stat = "identity", position = "dodge") +
+      geom_text(mapping = aes(x = Severity.Level, y = n, label = n), vjust = -0.75, size = 4) +
+      labs(
+        x = "Severity Level",
+        y = "Number of Attacks",
+        fill = "Severity Level",
+        title = "Severity Level Trends Based on Attack Type") + 
+      theme_classic()
+  })
+  
+  output$severe_corr <- renderPlot({
+    corr_df <- filter(cyb_df, Severity.Level == input$severe_type, Target.Variable == "Phishing")
+    
+    ggplot(corr_df) + 
+      geom_histogram(mapping = aes(x = Anomaly.Scores), stat = "bin", position = "stack", bins = 10, color = "black") +
+      labs(
+        x = "Anomaly Scores",
+        y = "Amount",
+        title = "What is the Frequency of Anomaly Scores per Severity of Attack?") +
+      theme_classic()
+  })
+  
+  output$phish_anom <- renderTable({
+    phish_df <- filter(cyb_df, Target.Variable == "Phishing", Anomaly.Scores >= input$anom_score[1], Anomaly.Scores <= input$anom_score[2]) %>%
+      select(Malware.Indicators) %>%
+      count(Malware.Indicators == "IoC Detected")
+    colnames(phish_df) <- c("Malware.Detected", "Amount")
+    phish_df$Malware.Detected[1] <- "Detected"
+    phish_df$Malware.Detected[2] <- "Undetected"
+    phish_df$Frequency[1] <- round( ( (phish_df$Amount[1]) / (phish_df$Amount[1] + phish_df$Amount[2]) ) * 100 )
+    phish_df$Frequency[2] <- round( ( (phish_df$Amount[2]) / (phish_df$Amount[1] + phish_df$Amount[2]) ) * 100 )
+    
+    phish_df
+  })
+  
+  output$phish_pckg <- renderTable({
+    phish_df2 <- filter(cyb_df, Target.Variable == "Phishing", AvgPacketPerEvent <= input$pckg.amt) %>%
+      select(Malware.Indicators) %>%
+      count(Malware.Indicators == "IoC Detected")
+    colnames(phish_df2) <- c("Malware.Detected", "Amount")
+    phish_df2$Malware.Detected[1] <- "Detected"
+    phish_df2$Malware.Detected[2] <- "Undetected"
+    phish_df2$Frequency[1] <- round( ( (phish_df2$Amount[1]) / (phish_df2$Amount[1] + phish_df2$Amount[2]) ) * 100 )
+    phish_df2$Frequency[2] <- round( ( (phish_df2$Amount[2]) / (phish_df2$Amount[1] + phish_df2$Amount[2]) ) * 100 )
+    
+    phish_df2
+  })
+  
 }
 
 shinyApp(ui = ui, server = server)
